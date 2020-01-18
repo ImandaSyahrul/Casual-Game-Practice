b87a994851c09531cded305e7956929023c1a07e
ImandaSyahrul <ImandaSyahrul@github.com>
2020-01-18 22:25:34 +0700

    First commit, currently working on main menu scene

---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public void MenuScene()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void GameScene()
+	{
+		SceneManager.LoadScene("GameScene");
+	}
+
+	public void QuitGame()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
