9eb6438c2eeaa07272174b04b67d94d043fd22e6
ImandaSyahrul <ImandaSyahrul@github.com>
2020-01-19 19:48:17 +0700

    Add platform menu loader script
    the script will enable the menu layout based on the platform(android, desktop, unity editor)

---
+++ Assets/Scripts/MenuLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLoader : MonoBehaviour
+{
+    [SerializeField] private GameObject androidMenu;
+    [SerializeField] private GameObject desktopMenu;
+    //Awake is called even the object is not enabled yet
+    private void Awake()
+    {
+        androidMenu.SetActive(false);
+        desktopMenu.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_ANDROID
+        androidMenu.SetActive(true);
+#else
+        desktopMenu.SetActive(true);
+#endif
+    }
+
+}
